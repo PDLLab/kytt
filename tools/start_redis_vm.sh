123df73ce1bd80f9669f99304aae36b5de694c5e
Aaron Zhang <chengfeizh@gmail.com>
2017-05-04 20:11:16 +0800

    update project
    1. conf: add Dockerfile for kytt
    2. conf: modify redis.conf, remove some configure
    3. tools: add start_mysql_vm script

--- tools/start_redis_vm.sh
+++ tools/start_redis_vm.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
-tag=$(docker images | grep redis | awk '{print $2}')
-if [ $tag = "3.0" ]; then
+# 准备镜像
+tag=$(docker images | grep redis | grep kytt | wc -l)
+if [ $tag = 1 ]; then
 	echo "Image Exist"
 else
 	echo "Image Not Exist"
-	echo "Pulling redis:3.0 Image"
-	docker pull redis:3.0
+	echo "Build redis:kytt Image"
+	cd ../conf
+	docker build -t "redis:kytt" -f Dockerfile.Redis.kytt .
 fi
 
+# 清除已经启动的redis_server容器
+docker ps -a | grep redis_server
+if [ $? = 0 ]; then
+	echo "Force Remove redis_server Container"
+	docker stop redis_server
+	docker rm redis_server
+fi
 
+# 启动redis_server容器
+cd ../
+echo $(pwd)
+echo "Start Redis Server Container"
+#docker run --name redis_server -v $(pwd)/data/redis_db:/data -v $(pwd)/conf/redis.conf:/usr/local/etc/redis/redis.conf redis:3.2.8 redis-server /usr/local/etc/redis/redis.conf
+##docker run --name redis_server -d -v $(pwd)/data/redis_db:/var/lib/reids -v $(pwd)/log:/var/log/redis -v $(pwd)/conf/redis.conf:/usr/local/etc/redis/redis.conf redis:3.0 redis-server /usr/local/etc/redis/redis.conf
